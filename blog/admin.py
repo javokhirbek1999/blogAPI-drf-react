d8af32ff6bde232e53e1a72d9c09c51c2bb58577
Javokhirbek <khaydaraliev99@gmail.com>
2021-07-29 09:12:43 +0300

    Added models for Post and Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category
+
+@admin.register(Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title','id','slug','author')
+    prepopulated_fields = {'slug':('title',),}
+
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    status_options = (
+        ('published','published'),
+        ('draft','draft'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
+    status = models.CharField(max_length=200, choices=status_options, default='draft')
+
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    class Meta:
+        ordering = ('-published',)
+    
+    def __str__(self):
+        return self.title
